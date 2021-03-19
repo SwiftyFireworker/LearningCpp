ffd586c03c23cb14702a86601de07385958c961c
Denisov Egor <egordenisov1505@mail.ru>
2021-03-20 01:21:09 +0300

    Uncommented

--- LearningCpp/Sem 7-8/Vector vs Set.cpp
+++ LearningCpp/Sem 7-8/Vector vs Set.cpp
@@ -1,40 +1,40 @@
-////
-////  main.cpp
-////  LearningCpp
-////
-////  Created by Egor Denisov on 10.02.2021.
-////
 //
-//#include <iostream>
-//#include <vector>
-//#include <set>
-//#include <ctime>
+//  main.cpp
+//  LearningCpp
 //
-//using namespace std;
+//  Created by Egor Denisov on 10.02.2021.
 //
-//
-//int main() {
-//    const int n = 100000;
-//    int numbers[n];
-//    for(int i = 0; i < n; i++) {
-//        numbers[i] = rand() % 100;
-//    }
-//    
-//    set<int> s;
-//    clock_t time = clock();
-//    for(int i = 0; i < n; i++) {
-//        s.insert(numbers[i]);
-//    }
-//    cout << "Set: " << clock() - time << endl;
-//    
-//    vector<int> v;
-//    for(int i = 0; i < n; i++) {
-//        v.push_back(numbers[i]);
-//    }
-//    time = clock();
-//    sort(v.begin(), v.end());
-//    cout << "Vector: " << clock() - time << endl;
-//    
-//    //LEADER: VECTOR
-//    return 0;
-//}
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <ctime>
+
+using namespace std;
+
+
+int main() {
+    const int n = 100000;
+    int numbers[n];
+    for(int i = 0; i < n; i++) {
+        numbers[i] = rand() % 100;
+    }
+    
+    set<int> s;
+    clock_t time = clock();
+    for(int i = 0; i < n; i++) {
+        s.insert(numbers[i]);
+    }
+    cout << "Set: " << clock() - time << endl;
+    
+    vector<int> v;
+    for(int i = 0; i < n; i++) {
+        v.push_back(numbers[i]);
+    }
+    time = clock();
+    sort(v.begin(), v.end());
+    cout << "Vector: " << clock() - time << endl;
+    
+    //LEADER: VECTOR
+    return 0;
+}
--- LearningCpp/Sem 9-10/fill_multi_array.cpp
+++ LearningCpp/Sem 9-10/fill_multi_array.cpp
@@ -1,133 +1,133 @@
-////
-////  fill_multi_array.cpp
-////  LearningCpp
-////
-////  Created by Egor Denisov on 11.03.2021.
-////
 //
-//#include <iostream>
-//#include <vector>
-//#include <boost/multi_array.hpp>
+//  fill_multi_array.cpp
+//  LearningCpp
 //
+//  Created by Egor Denisov on 11.03.2021.
 //
-////template<typename T, typename U, std::size_t Dim, std::size_t Dim2>
-////void fill_multi_array(typename SubArray<T, Dim, Dim2>::type multiArray, const std::vector<U> & vec) {
-////    if (Dim == 1) {
-////        for(size_t i = 0; i < vec.size(); i++) {
-////            multiArray[i] = vec[i];
-////        }
-////    } else {
-////        for(size_t i = 0; i < vec.size(); i++) {
-////            fill_multi_array(multiArray[i], vec[i]);
-////        }
-////    }
-////}
-//
-////template<typename T>
-////struct is_vector: std::false_type
-////{  };
-////
-////template<typename T>
-////struct is_vector<std::vector<T>>: std::true_type
-////{  };
-//
-//template<typename T, typename U, std::size_t Dim>
-//void fill_multi_array(boost::detail::multi_array::sub_array<T, Dim> & multiArray, const std::vector<U> & vec) {
-//    if (Dim > 1) {
-//        for(size_t i = 0; i < vec.size(); i++) {
-//            fill_multi_array(multiArray[i], vec[i]);
-//        }
-////        return;
-//    } else {
+
+#include <iostream>
+#include <vector>
+#include <boost/multi_array.hpp>
+
+
+//template<typename T, typename U, std::size_t Dim, std::size_t Dim2>
+//void fill_multi_array(typename SubArray<T, Dim, Dim2>::type multiArray, const std::vector<U> & vec) {
+//    if (Dim == 1) {
 //        for(size_t i = 0; i < vec.size(); i++) {
 //            multiArray[i] = vec[i];
 //        }
+//    } else {
+//        for(size_t i = 0; i < vec.size(); i++) {
+//            fill_multi_array(multiArray[i], vec[i]);
+//        }
 //    }
 //}
+
+//template<typename T>
+//struct is_vector: std::false_type
+//{  };
 //
-////template<typename T, typename U, std::size_t Dim=1>
-////void fill_multi_array(boost::detail::multi_array::sub_array<T, Dim> & multiArray, const std::vector<U> & vec) {
-////    for(size_t i = 0; i < vec.size(); i++) {
-////        multiArray[i] = vec[i];
-////    }
-////}
-//
+//template<typename T>
+//struct is_vector<std::vector<T>>: std::true_type
+//{  };
+
+template<typename T, typename U, std::size_t Dim>
+void fill_multi_array(boost::detail::multi_array::sub_array<T, Dim> & multiArray, const std::vector<U> & vec) {
+    if (Dim > 1) {
+        for(size_t i = 0; i < vec.size(); i++) {
+            fill_multi_array(multiArray[i], vec[i]);
+        }
+//        return;
+    } else {
+        for(size_t i = 0; i < vec.size(); i++) {
+            multiArray[i] = vec[i];
+        }
+    }
+}
+
+//template<typename T, typename U, std::size_t Dim=1>
+//void fill_multi_array(boost::detail::multi_array::sub_array<T, Dim> & multiArray, const std::vector<U> & vec) {
+//    for(size_t i = 0; i < vec.size(); i++) {
+//        multiArray[i] = vec[i];
+//    }
+//}
+
+template<typename T, typename U, std::size_t Dim>
+void fill_multi_array(boost::multi_array<T, Dim> & multiArray, const std::vector<U> & vec) {
+    if (Dim > 1) {
+        for(size_t i = 0; i < vec.size(); i++) {
+            fill_multi_array(multiArray[i], vec[i]);
+        }
+        return;
+    } else {
+        for(size_t i = 0; i < vec.size(); i++) {
+            multiArray[i] = vec[i];
+        }
+    }
+}
+
+
+//template<typename T, typename U>
+//void fill_multi_array(T & multiArray, const U & vec) {
+//    if (is_vector<U>::value) {
+//        for(size_t i = 0; i < vec.size(); i++) {
+//            multiArray[i] = vec[i];
+//        }
+//    } else {
+//        for(size_t i = 0; i < vec.size(); i++) {
+//            fill_multi_array(multiArray[i], vec[i]);
+//        }
+//    }
+//}
+
 //template<typename T, typename U, std::size_t Dim>
 //void fill_multi_array(boost::multi_array<T, Dim> & multiArray, const std::vector<U> & vec) {
-//    if (Dim > 1) {
-//        for(size_t i = 0; i < vec.size(); i++) {
-//            fill_multi_array(multiArray[i], vec[i]);
-//        }
-//        return;
-//    } else {
+//    if (Dim == 1) {
 //        for(size_t i = 0; i < vec.size(); i++) {
 //            multiArray[i] = vec[i];
 //        }
-//    }
-//}
-//
-//
-////template<typename T, typename U>
-////void fill_multi_array(T & multiArray, const U & vec) {
-////    if (is_vector<U>::value) {
-////        for(size_t i = 0; i < vec.size(); i++) {
-////            multiArray[i] = vec[i];
-////        }
-////    } else {
-////        for(size_t i = 0; i < vec.size(); i++) {
-////            fill_multi_array(multiArray[i], vec[i]);
-////        }
-////    }
-////}
-//
-////template<typename T, typename U, std::size_t Dim>
-////void fill_multi_array(boost::multi_array<T, Dim> & multiArray, const std::vector<U> & vec) {
-////    if (Dim == 1) {
-////        for(size_t i = 0; i < vec.size(); i++) {
-////            multiArray[i] = vec[i];
-////        }
-////    } else {
-////        for(size_t i = 0; i < vec.size(); i++) {
-////            fill_multi_array(static_cast<boost::multi_array<T, Dim-1>>(multiArray[i]), vec[i]);
-////        }
-////    }
-////}
-//
-//int main() {
-//    {
-//        std::vector<double> vec {1, 2, 3};
-//        boost::multi_array<double, 1> arr { boost::extents[3] };
-//        fill_multi_array(arr, vec);
-//        for (int i = 0; i < 3; i++) {
-//            if (arr[i] != vec[i]) {
-//                std::cout << "Not ok: " << arr[i] << " " << vec[i] << std::endl;
-//            }
+//    } else {
+//        for(size_t i = 0; i < vec.size(); i++) {
+//            fill_multi_array(static_cast<boost::multi_array<T, Dim-1>>(multiArray[i]), vec[i]);
 //        }
 //    }
+//}
+
+int main() {
+    {
+        std::vector<double> vec {1, 2, 3};
+        boost::multi_array<double, 1> arr { boost::extents[3] };
+        fill_multi_array(arr, vec);
+        for (int i = 0; i < 3; i++) {
+            if (arr[i] != vec[i]) {
+                std::cout << "Not ok: " << arr[i] << " " << vec[i] << std::endl;
+            }
+        }
+    }
+    {
+        std::vector<std::vector<double>> vec { {1, 2, 3}, {4, 5, 6} };
+        boost::multi_array<double, 2> arr { boost::extents[3][3] };
+        fill_multi_array(arr, vec);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (arr[i][j] != vec[i][j]) {
+                    std::cout << "Not ok: " << arr[i][j] << " " << vec[i][j] << std::endl;
+                }
+            }
+        }
+    }
 //    {
-//        std::vector<std::vector<double>> vec { {1, 2, 3}, {4, 5, 6} };
-//        boost::multi_array<double, 2> arr { boost::extents[3][3] };
+//        std::vector<std::vector<std::vector<double>>> vec { { {1, 2, 3}, {4, 5, 6} } };
+//        boost::multi_array<double, 3> arr { boost::extents[3][3] };
 //        fill_multi_array(arr, vec);
-//        for (int i = 0; i < 2; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                if (arr[i][j] != vec[i][j]) {
-//                    std::cout << "Not ok: " << arr[i][j] << " " << vec[i][j] << std::endl;
+//        for (int k = 0; k < 1; k++) {
+//            for (int i = 0; i < 2; i++) {
+//                for (int j = 0; j < 3; j++) {
+//                    if (arr[k][i][j] != vec[k][i][j]) {
+//                        std::cout << "Not ok: " << arr[k][i][j] << " " << vec[k][i][j] << std::endl;
+//                    }
 //                }
 //            }
 //        }
 //    }
-////    {
-////        std::vector<std::vector<std::vector<double>>> vec { { {1, 2, 3}, {4, 5, 6} } };
-////        boost::multi_array<double, 3> arr { boost::extents[3][3] };
-////        fill_multi_array(arr, vec);
-////        for (int k = 0; k < 1; k++) {
-////            for (int i = 0; i < 2; i++) {
-////                for (int j = 0; j < 3; j++) {
-////                    if (arr[k][i][j] != vec[k][i][j]) {
-////                        std::cout << "Not ok: " << arr[k][i][j] << " " << vec[k][i][j] << std::endl;
-////                    }
-////                }
-////            }
-////        }
-////    }
-//}
+}
